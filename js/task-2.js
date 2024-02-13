8517b5a8b7952be967c47e39b06a91dc61f311b8
Bark1s <147616559+Bark1s@users.noreply.github.com>
2024-02-13 12:17:37 -0500

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,15 @@
+function makeArray(firstArray, secondArray, maxLength) {
+    let newArray = firstArray.concat(secondArray);
+    let finalArray = [];
+    for (let count = 0; count < maxLength; count += 1) {
+        finalArray.push(newArray[count])
+    }
+    return finalArray;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
